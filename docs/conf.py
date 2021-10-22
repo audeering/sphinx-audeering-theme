c800f4e96f7313152e498c30a4f63b90d543e6f5
Hagen Wierstorf <hwierstorf@audeering.com>
2021-10-22 15:35:46 +0200

    Update theme to new audEERING homepage (#34)

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,6 @@ master_doc = 'index'
 extensions = []
 source_suffix = '.rst'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-pygments_style = None
 extensions = [
     'sphinx.ext.autodoc',
 ]
